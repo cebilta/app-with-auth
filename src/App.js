ba42fdccbd0daf11e7873c48ec59745c58aaad5a
cebilta <53847161+cebilta@users.noreply.github.com>
2021-02-18 12:42:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
-import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
 
-Amplify.configure(awsconfig);
 
 function App() {
   return (
