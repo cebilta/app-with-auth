5a334d35cb60a92bfe469cc407d01aa751aea6d1
cebilta <53847161+cebilta@users.noreply.github.com>
2021-02-18 12:43:03 +0530

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "../../App.css";
-import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
-import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+
 
 function SignUp() {
   return (
     <div>
-      <AmplifySignOut />
+      
     </div>
   );
 }
-export default withAuthenticator(SignUp);
+export default SignUp;
